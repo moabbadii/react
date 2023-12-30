c58f82751debf82309bc088bd349ea891eabfeb6
moabbadii <127883009+moabbadii@users.noreply.github.com>
2023-12-30 22:14:30 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+const API_KEY = 'e428100e8beb358ab9d3eb77c7bc35b7';
+
+function App() {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState(null);
+
+  const getWeather = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      console.log('Weather data:', response.data);
+      setWeather(response.data);
+      setError(null);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+      setWeather(null);
+      setError('Error fetching weather data. Please try again.');
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter city"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button onClick={getWeather}>Get Weather</button>
+      </div>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {weather && (
+        <div>
+          <h2>{weather.name}, {weather.sys.country}</h2>
+          <p>Temperature: {weather.main.temp} °C</p>
+          <p>Weather: {weather.weather[0].description}</p>
+          <p>Humidity: {weather.main.humidity}%</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
